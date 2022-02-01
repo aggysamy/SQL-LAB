a32e27430754fc27e036e178d4b92e9e9efa2146
Agnes <97636191+aggysamy@users.noreply.github.com>
2022-02-01 12:25:36 +0100

    Add files via upload

---
+++ SQL !.sql
@@ -0,0 +1,10 @@
+use sakila; 
+select * from actor, film, customer;
+Select * from film;
+ select distinct film language from language as alias;
+ select count * (type) store;
+select count * (type) employees;
+select * into employees from first names 
+
+
+
